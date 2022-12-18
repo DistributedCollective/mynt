a4824d8f3a9b017f4ef2c5d9e1b27629df34f4f0
Tyrone Johnson <tjcloa@gmail.com>
2022-12-18 23:45:19 +0300

    refactor MocIntegration: natspec, renaming, chore

--- contracts/integration/MoC/MocIntegration.sol
+++ contracts/integration/MoC/MocIntegration.sol
@@ -7,14 +7,7 @@ import "../../meta-asset-token/DLLR.sol";
 import "../../interfaces/IMassetManager.sol";
 import "../../interfaces/IDLLR.sol";
 
-// Here you will import your own dependencies
-
-// > DLLR withdraw DOC to the contract by permit
-// > the contract calls IMoC to redeem DoC for RBTC
-// >> if redemption by settlement then the user can call redemption later on this contract (security!?)
-// >>> check if there is an option to redeem from this contract to the user
-// >> else transfer DoC directly to the user
-
+/// @notice This contract provides compound functions with Money On Chain wrapping them in one transaction for convenience and to save on gas
 contract MocIntegration {
     // Address of the MoC contract
     IMocMintRedeemDoc public immutable moc;
@@ -22,6 +15,8 @@ contract MocIntegration {
     IERC20 public immutable doc;
     IDLLR public immutable dllr;
     IMassetManager public immutable massetManager;
+
+    event GetDocFromDllrAndRedeemRBTC(uint256 fromDLLR, uint256 toRBTC);
 
     /**
      * @param _moc MoC main contract address
@@ -41,49 +36,52 @@ contract MocIntegration {
     }
 
     /**
-     * @notice Convert DLLR _amount to DoC utilizing EIP-2612 permit
-     * to reduce the additional sending transaction for doing the approval to the spender.
+     * @notice how getDocFromDllrAndRedeemRBTC function works:
+     * -------------------------------------------------------------------------------------------
+     * |               Mynt                         |                Money On Chain              |
+     * -------------------------------------------------------------------------------------------
+     * | Get DLLR (EIP-2612) -> convert DLLR to DoC | -> get RBTC from DLLR -> send RBTC to user |
+     * -------------------------------------------------------------------------------------------
      *
-     * @param _amount The amount of the DLLR (mAsset) token that will be burned in exchange for _toToken
+     * @param _dllrAmount The amount of the DLLR (mAsset) that will be burned in exchange for _toToken
      * @param _permitParams EIP-2612 permit params:
      *        _deadline Expiration time of the signature.
      *        _v Last 1 byte of ECDSA signature.
      *        _r First 32 bytes of ECDSA signature.
      *        _s 32 bytes after _r in ECDSA signature.
-     * @param _redeemFree MoC redeem has 2 options - redeem free DoC or create redeem request //TODO: add link to the description
      */
-    function getDocFromDllrAndRedeemRBTC(
-        uint256 _amount,
-        PermitParams calldata _permitParams,
-        bool _redeemFree
-    ) external {
-        // transfer DLLR _amount to this contract by permit ()
+    function getDocFromDllrAndRedeemRBTC(uint256 _dllrAmount, PermitParams calldata _permitParams) external {
+        // transfer _dllrAmount to this contract by permit (EIP-2612)
         address thisAddress = address(this);
         uint256 dllrBalanceBefore = dllr.balanceOf(thisAddress);
         dllr.transferWithPermit(
             msg.sender,
             thisAddress,
-            _amount,
+            _dllrAmount,
             _permitParams.deadline,
             _permitParams.v,
             _permitParams.r,
             _permitParams.s
         );
         require(
-            dllr.balanceOf(thisAddress) == dllrBalanceBefore + _amount,
+            dllr.balanceOf(thisAddress) == dllrBalanceBefore + _dllrAmount,
             "MocIntegration:: DLLR transfer with permit received wrong amount"
         );
 
         // redeem DoC from DLLR
         require(
-            massetManager.redeemTo(address(doc), _amount, thisAddress) == _amount,
+            massetManager.redeemTo(address(doc), _dllrAmount, thisAddress) == _dllrAmount,
             "MocIntegration:: redeemed incorrect DoC amount"
         );
 
         // redeem RBTC from DoC using Money On Chain and send to the user
-        uint256 rbtcBalanceBefore = thisAddress.balance; //doc.balanceOf(thisAddress);
-        moc.redeemFreeDocVendors(_amount, payable(address(0)));
-        (bool success, ) = msg.sender.call{ value: thisAddress.balance - rbtcBalanceBefore }("");
+        uint256 rbtcBalanceBefore = thisAddress.balance;
+        // TODO: clarify the vendor address param
+        moc.redeemFreeDocVendors(_dllrAmount, payable(address(0)));
+        uint256 rbtcAmount = thisAddress.balance - rbtcBalanceBefore;
+        (bool success, ) = msg.sender.call{ value: rbtcAmount }("");
         require(success, "MocIntegration:: error transferring redeemed RBTC");
+
+        emit GetDocFromDllrAndRedeemRBTC(_dllrAmount, rbtcAmount);
     }
 }
