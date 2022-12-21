b791ecb597f6586e57a2ee71466f6b0984cb8889
Santo Sinar Pandean <santo.pandean@gmail.com>
2022-12-21 10:15:50 +0700

    use hh deploy log
    change max and min ratio for bassets to 0

--- deploy/7_AddBassetInBasketManager.ts
+++ deploy/7_AddBassetInBasketManager.ts
@@ -6,25 +6,36 @@ const func: DeployFunction = async ({
   getNamedAccounts,
 }) => {
   const network = deployments.getNetworkName();
+  const log = deployments.log;
   const {deployer} = await getNamedAccounts();
-  let bAssets = [
-    "0x6b41566353d6c7b8c2a7931d498f11489dacac29", // ZUSD Testnet
-    "0xcb46c0ddc60d18efeb0e586c17af6ea36452dae0", // DOC Tesnet
-  ];
+  let bAssets;
   let factors = [1,1];
   let bridges = [constants.AddressZero, constants.AddressZero];
-  let mins = [10,10];
-  let maxs = [100,100];
+  let mins = [0,0];
+  let maxs = [0,0];
   let pausedFlags = [false, false];
 
-  if(network === 'rskMainnet') {
+  if(network === 'development' || network === 'rskTestnet') {
+    bAssets = [
+      "0x6b41566353d6c7b8c2a7931d498f11489dacac29", // ZUSD Testnet
+      "0xcb46c0ddc60d18efeb0e586c17af6ea36452dae0", // DOC Tesnet
+    ];
+  } else if(network === 'rskMainnet') {
     bAssets = [
       "0xdb107fa69e33f05180a4c2ce9c2e7cb481645c2d", // ZUSD Mainnet
       "0xe700691da7b9851f2f35f8b8182c69c53ccad9db" // DOC Mainnet
     ];
   }
 
-  console.log("adding bAssets...")
+  log("adding bAssets with details: ", JSON.stringify({
+    bAssets,
+    factors,
+    bridges,
+    mins,
+    maxs,
+    pausedFlags
+  }));
+
   await deployments.execute(
     "BasketManagerV3",
     { from: deployer },
@@ -36,7 +47,8 @@ const func: DeployFunction = async ({
     maxs,
     pausedFlags
   );
-  console.log("done")
+
+  log("done");
 };
 
 func.tags = ["AddBassetsInBasketManager"];
