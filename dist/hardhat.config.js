e1bd29c46ceaa19ddd439199ef0fda89a24cfacd
cwsnt <udefined@udefinedd.com>
2024-12-13 22:33:46 +0700

    fix: basset removal

--- dist/hardhat.config.js
+++ dist/hardhat.config.js
@@ -40,7 +40,10 @@ require("hardhat-contract-sizer");
 require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-solhint");
 require("./tasks");
-require("node_modules/sovrynsmartcontracts/hardhat/tasks/multisig");
+//@todo - resolve conflicts in "sovrynsmartcontracts": "git+https://github.com/DistributedCollective/Sovryn-smart-contracts.git"
+// install the package
+// uncomment the line below to use multisig scripts from the core repo
+// import "node_modules/sovrynsmartcontracts/hardhat/tasks/multisig";
 /*
  * Test hardhat forking with patched hardhat
  *
